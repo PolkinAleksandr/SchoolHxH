0e77142b3e4c126c6e05ccb5656eed51208b18c0
PolkinAleksandr <metuct@mail.ru>
2023-10-19 15:26:45 +0300

    test commit

---
+++ MainActivity/app/src/main/java/com/example/mainactivity/FirstFragment.kt
@@ -0,0 +1,43 @@
+package com.example.mainactivity
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.postDelayed(
+            {
+                findNavController().navigate(FirstFragmentDirections.actionFirstFragmentToSecondFragment("asd"))
+            },
+            500
+        )
+    }
+
+    companion object {
+        fun newInstance() =
+            FirstFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
